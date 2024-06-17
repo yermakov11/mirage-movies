21e7c6e2b0796222f38bbc8b54a3a52016092457
unknown <thumpel81@gmail.com>
2024-06-17 20:18:33 +0100

    changed main

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './index.css';
-import MainPage from './pages/MainPage';
-import MoviePage from './pages/MoviePage';
-import { Route,Routes } from 'react-router-dom';
-function App() { 
- return (
+import "./index.css";
+import MainPage from "./pages/MainPage";
+import MoviePage from "./pages/MoviePage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+function App() {
+  return (
     <div>
-      <Routes>
-        <Route path='/mirage-movies/' element={ <MainPage/>}/>
-        <Route path='/movie/:movieId' element={<MoviePage/>}/>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/mirage-movies/" element={<MainPage />} />
+          <Route path="/movie/:movieId" element={<MoviePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
-export default App
+export default App;
